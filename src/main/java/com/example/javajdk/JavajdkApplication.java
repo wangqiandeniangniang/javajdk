e71a3d49a6d54f471ccced662420319dd957f720
chen.liang <cl123456>
2018-11-13 10:09:45 +0800

    first commit

---
+++ src/main/java/com/example/javajdk/JavajdkApplication.java
@@ -0,0 +1,12 @@
+package com.example.javajdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavajdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavajdkApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/javajdk/JavajdkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.javajdk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavajdkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
