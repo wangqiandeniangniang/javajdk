6e99fb2bef2a3b9f77c1d40232dd3a9245a718d5
chen.liang <cl123456>
2018-11-29 10:56:47 +0800

    实现有序的list的抽象类

--- src/main/java/com/example/javajdk/srcjdk/java/util/AbstractSequentialList.java
+++ src/main/java/com/example/javajdk/srcjdk/java/util/AbstractSequentialList.java
@@ -23,7 +23,12 @@
  *
  */
 
-package java.util;
+package com.example.javajdk.srcjdk.java.util;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
 
 /**
  * This class provides a skeletal implementation of the <tt>List</tt>
@@ -31,23 +36,27 @@ package java.util;
  * backed by a "sequential access" data store (such as a linked list).  For
  * random access data (such as an array), <tt>AbstractList</tt> should be used
  * in preference to this class.<p>
+ * 有序list
  *
- * This class is the opposite of the <tt>AbstractList</tt> class in the sense
+ * This class is the opposite【相对的】 of the <tt>AbstractList</tt> class in the sense
  * that it implements the "random access" methods (<tt>get(int index)</tt>,
  * <tt>set(int index, E element)</tt>, <tt>add(int index, E element)</tt> and
  * <tt>remove(int index)</tt>) on top of the list's list iterator, instead of
- * the other way around.<p>
+ * the other way around[相反的方向].<p>
+ *  在AbstractList基础上 实现 get/set/add/remove
  *
  * To implement a list the programmer needs only to extend this class and
  * provide implementations for the <tt>listIterator</tt> and <tt>size</tt>
  * methods.  For an unmodifiable list, the programmer need only implement the
  * list iterator's <tt>hasNext</tt>, <tt>next</tt>, <tt>hasPrevious</tt>,
  * <tt>previous</tt> and <tt>index</tt> methods.<p>
+ *需要实现listIterator和size方法，对于不变的list，程序员需要实现list的hasNext、next、hasPrevious previous,index
  *
  * For a modifiable list the programmer should additionally implement the list
  * iterator's <tt>set</tt> method.  For a variable-size list the programmer
  * should additionally implement the list iterator's <tt>remove</tt> and
  * <tt>add</tt> methods.<p>
+ *  对于可变的list set方法， 对于可变长度需要增加remove和add方法
  *
  * The programmer should generally provide a void (no argument) and collection
  * constructor, as per the recommendation in the <tt>Collection</tt> interface
@@ -71,15 +80,17 @@ public abstract class AbstractSequentialList<E> extends AbstractList<E> {
      * Sole constructor.  (For invocation by subclass constructors, typically
      * implicit.)
      */
-    protected AbstractSequentialList() {
+    protected AbstractSequentialList() { //空构造方法
     }
 
     /**
      * Returns the element at the specified position in this list.
      *
+     * 返回某个索引位置的列表， 这个返回索引位置的指针，然后通过next获取下一个元素
      * <p>This implementation first gets a list iterator pointing to the
      * indexed element (with <tt>listIterator(index)</tt>).  Then, it gets
      * the element using <tt>ListIterator.next</tt> and returns it.
+     *
      *
      * @throws IndexOutOfBoundsException {@inheritDoc}
      */
@@ -87,7 +98,7 @@ public abstract class AbstractSequentialList<E> extends AbstractList<E> {
         try {
             return listIterator(index).next();
         } catch (NoSuchElementException exc) {
-            throw new IndexOutOfBoundsException("Index: "+index);
+            throw new IndexOutOfBoundsException("Index: "+index);// 如果没有该元素抛出异常
         }
     }
 
@@ -95,11 +106,13 @@ public abstract class AbstractSequentialList<E> extends AbstractList<E> {
      * Replaces the element at the specified position in this list with the
      * specified element (optional operation).
      *
+     * 替换索引位置的元素，然后返回该元素，替换元素
      * <p>This implementation first gets a list iterator pointing to the
      * indexed element (with <tt>listIterator(index)</tt>).  Then, it gets
      * the current element using <tt>ListIterator.next</tt> and replaces it
      * with <tt>ListIterator.set</tt>.
      *
+     * 如果没有实现set方法将会抛出UnsupportedOperationException
      * <p>Note that this implementation will throw an
      * <tt>UnsupportedOperationException</tt> if the list iterator does not
      * implement the <tt>set</tt> operation.
@@ -113,11 +126,11 @@ public abstract class AbstractSequentialList<E> extends AbstractList<E> {
     public E set(int index, E element) {
         try {
             ListIterator<E> e = listIterator(index);
-            E oldVal = e.next();
-            e.set(element);
+            E oldVal = e.next(); //获取该元素
+            e.set(element);//设置元素
             return oldVal;
-        } catch (NoSuchElementException exc) {
-            throw new IndexOutOfBoundsException("Index: "+index);
+        } catch (NoSuchElementException exc) { // 没有这样的元素
+            throw new IndexOutOfBoundsException("Index: "+index); //抛出索引越界
         }
     }
 
@@ -126,11 +139,13 @@ public abstract class AbstractSequentialList<E> extends AbstractList<E> {
      * (optional operation).  Shifts the element currently at that position
      * (if any) and any subsequent elements to the right (adds one to their
      * indices).
+     * 在某个位置插入元素，需要实现add方法
      *
      * <p>This implementation first gets a list iterator pointing to the
      * indexed element (with <tt>listIterator(index)</tt>).  Then, it
      * inserts the specified element with <tt>ListIterator.add</tt>.
      *
+     *  需要实现 add方法，否则抛出UnsupportedOperationException
      * <p>Note that this implementation will throw an
      * <tt>UnsupportedOperationException</tt> if the list iterator does not
      * implement the <tt>add</tt> operation.
@@ -144,7 +159,7 @@ public abstract class AbstractSequentialList<E> extends AbstractList<E> {
     public void add(int index, E element) {
         try {
             listIterator(index).add(element);
-        } catch (NoSuchElementException exc) {
+        } catch (NoSuchElementException exc) { //
             throw new IndexOutOfBoundsException("Index: "+index);
         }
     }
@@ -154,6 +169,7 @@ public abstract class AbstractSequentialList<E> extends AbstractList<E> {
      * operation).  Shifts any subsequent elements to the left (subtracts one
      * from their indices).  Returns the element that was removed from the
      * list.
+     * 移除特定位置的元素，如果没有实现remove 将会抛出 UnsupportedOperationException 异常
      *
      * <p>This implementation first gets a list iterator pointing to the
      * indexed element (with <tt>listIterator(index)</tt>).  Then, it removes
@@ -173,7 +189,7 @@ public abstract class AbstractSequentialList<E> extends AbstractList<E> {
             e.remove();
             return outCast;
         } catch (NoSuchElementException exc) {
-            throw new IndexOutOfBoundsException("Index: "+index);
+            throw new IndexOutOfBoundsException("Index: "+index); //索引越界
         }
     }
 
@@ -190,6 +206,7 @@ public abstract class AbstractSequentialList<E> extends AbstractList<E> {
      * undefined if the specified collection is modified while the
      * operation is in progress.  (Note that this will occur if the specified
      * collection is this list, and it's nonempty.)
+     *  插入特定元素到特定位置中，移动当前位置，这些新元素将会出现列表中有序的
      *
      * <p>This implementation gets an iterator over the specified collection and
      * a list iterator over this list pointing to the indexed element (with
@@ -203,6 +220,7 @@ public abstract class AbstractSequentialList<E> extends AbstractList<E> {
      * the <tt>listIterator</tt> method does not implement the <tt>add</tt>
      * operation.
      *
+     * 有序并不是从大到小的，索引插入位置没有限制
      * @throws UnsupportedOperationException {@inheritDoc}
      * @throws ClassCastException            {@inheritDoc}
      * @throws NullPointerException          {@inheritDoc}
